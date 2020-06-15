db73373aab7f0431b9ead05a0e9930ad50f2761b
nguyendangbanghn93 <55304669+nguyendangbanghn93@users.noreply.github.com>
2020-06-15 21:29:38 +0700

    Add yolo text

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace ConsoleApplication3
+﻿using System;
+
+namespace ConsoleApplication3
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Yolo git");
         }
     }
 }
